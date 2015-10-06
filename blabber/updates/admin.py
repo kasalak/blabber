88dd091dc99f878ae00466d0c11bf44801161f11
Karthik <karthik.kasala@gmail.com>
2015-10-06 12:30:27 -0400

    added status object

--- blabber/updates/admin.py
+++ blabber/updates/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Status, Favorite
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['user', 'text']
 
 # Register your models here.
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Favorite)
